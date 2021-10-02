af0ca7520c3519c43a621cde62e6db8667557b82
Abhishek Jha <66509743+abhishek5670@users.noreply.github.com>
2021-10-02 20:56:34 +0530

    Update 1.py
    i have added a program in your repo that's you want if it is correct then please merge this program with your repo

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
 # YOU HAVE TO WRITE CODE HERE.... 
 # GIVE A PROPER NAME TO YOUR PROGRAM...
+#Print list in reverse order using a loop
+list = []
+n = int(input("enter the number in the list\n"))
+for i in range (n):
+    ele = input("enter the element\n")
+    list.append(ele)
+print(list)
+j = len(list) -1
+list1 = []
+while j >=0:
+    list1.append(list[j])
+    j-=1
+print(list1)
